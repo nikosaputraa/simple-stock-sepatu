9038ce49f075022c2a92210b35e0127829a4ec79
nikosaputraa <nikosaputra890@gmail.com>
2024-07-02 20:04:02 +0700

    update db

--- stock_sepatu.sql
+++ stock_sepatu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2024 at 11:38 AM
+-- Generation Time: Jul 02, 2024 at 03:01 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -37,9 +37,7 @@ CREATE TABLE `kategori_produk` (
 --
 
 INSERT INTO `kategori_produk` (`id`, `kategori`) VALUES
-(1, 'Sepatu'),
-(3, 'Sepatu Cowok'),
-(4, 'Sepatu Running');
+(2, 'Sepatu Running');
 
 -- --------------------------------------------------------
 
@@ -61,10 +59,9 @@ CREATE TABLE `pengguna` (
 
 INSERT INTO `pengguna` (`id`, `username`, `password`, `nama`, `role`) VALUES
 (1, 'admin', '$2y$10$/I7laWi1mlNFxYSv54EUPOH8MuZhmRWxhE.LaddTK9TSmVe.IHP2C', 'Admin', '1'),
-(5, 'kiki', '$2y$10$kyckz5z04Nn93mWUSz/GneDEcWToeAV/2JcTpqTBz0Vrz8RLxqzfC', 'kiki', '2'),
 (8, 'niko', '$2y$10$xB6doRyM0HrLVj02vzF24e18sodIjMOAyPh9arkSUnCtUrYSM57Wu', 'Niko Saputra', '1'),
-(9, 'user', '$2y$10$GU9agXzlW.esAWf5xs9FNufu7vcxqCmeC9yKyXnAI7Pxw/PlDHzIq', 'User', '2'),
-(10, 'kasir', '$2y$10$lD.h100j0uM8ujWYd/cM4esbW63PglmDJR0/OvDLm.c1HU2iALMCa', 'Kasir', '2');
+(10, 'kasir', '$2y$10$lD.h100j0uM8ujWYd/cM4esbW63PglmDJR0/OvDLm.c1HU2iALMCa', 'Kasir', '2'),
+(12, 'kasir-niko', '$2y$10$KmkDMa.dQqcsnJy8emTqtuMcF4AJxkMbmEKAfkURu9v.g0spzImuC', 'Kasir Niko', '2');
 
 -- --------------------------------------------------------
 
@@ -88,10 +85,7 @@ CREATE TABLE `produk` (
 --
 
 INSERT INTO `produk` (`id`, `barcode`, `nama_produk`, `kategori`, `satuan`, `harga`, `stok`, `terjual`) VALUES
-(1, 'Cnvrs', 'Converse 70\'s Egret High', 1, 1, '650000', 93, '2'),
-(2, 'Ardls', 'Ardiles Back to School', 1, 1, '250000', 157, '3'),
-(5, 'Amk', 'Amikom', 3, 1, '2000000', 10, ''),
-(6, 'Sepatu Adidas', 'Sepatu Adidas', 4, 1, '500000', 371, '');
+(1, 'ADS_SMB', 'Sepatu Adidas Samba', 2, 1, '800000', 580, '');
 
 -- --------------------------------------------------------
 
@@ -109,8 +103,7 @@ CREATE TABLE `satuan_produk` (
 --
 
 INSERT INTO `satuan_produk` (`id`, `satuan`) VALUES
-(1, 'Sepasang'),
-(3, 'Pcs');
+(1, 'Sepasang');
 
 -- --------------------------------------------------------
 
@@ -131,7 +124,7 @@ CREATE TABLE `stok_keluar` (
 --
 
 INSERT INTO `stok_keluar` (`id`, `tanggal`, `barcode`, `jumlah`, `Keterangan`) VALUES
-(7, '2024-07-02 15:48:17', 6, '400', 'Rusak');
+(1, '2024-07-02 18:37:32', 1, '20', 'Deffect');
 
 -- --------------------------------------------------------
 
@@ -144,44 +137,15 @@ CREATE TABLE `stok_masuk` (
   `tanggal` datetime NOT NULL,
   `barcode` int(11) NOT NULL,
   `jumlah` varchar(11) NOT NULL,
-  `keterangan` varchar(10) NOT NULL,
-  `supplier` int(11) DEFAULT NULL
+  `keterangan` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `stok_masuk`
 --
 
-INSERT INTO `stok_masuk` (`id`, `tanggal`, `barcode`, `jumlah`, `keterangan`, `supplier`) VALUES
-(10, '2024-07-02 15:46:56', 6, '200', 'Stok Masuk', NULL),
-(11, '2024-07-02 16:05:21', 6, '25', 'Stok Masuk', NULL),
-(12, '2024-07-02 16:17:02', 7, '80', 'Stok Masuk', NULL),
-(13, '2024-07-02 16:19:54', 5, '10', 'Stok Masuk', NULL),
-(14, '2024-07-02 16:32:56', 2, '20', 'Stok Masuk', NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `supplier`
---
-
-CREATE TABLE `supplier` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `alamat` varchar(100) NOT NULL,
-  `telepon` varchar(15) NOT NULL,
-  `keterangan` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `supplier`
---
-
-INSERT INTO `supplier` (`id`, `nama`, `alamat`, `telepon`, `keterangan`) VALUES
-(1, 'PT.MAP', 'Jakarta', '00102012466', 'Distributor retail resmi sepatu converse'),
-(2, 'PT Wangta Agung', 'Jakarta', '0245369785', 'Distributor retail resmi ardiles'),
-(3, 'PT Vigano Cipta Perdana', 'Jakarta', '45269710230', 'Distributor retail resmi Buccheri'),
-(6, 'PT AMIKOM', 'YOGYAKARTA', '15423650579', 'Distributor Retail Resmi Amikom');
+INSERT INTO `stok_masuk` (`id`, `tanggal`, `barcode`, `jumlah`, `keterangan`) VALUES
+(1, '2024-07-02 18:36:37', 1, '600', 'Stok Masuk');
 
 -- --------------------------------------------------------
 
@@ -201,34 +165,6 @@ CREATE TABLE `toko` (
 
 INSERT INTO `toko` (`id`, `nama`, `alamat`) VALUES
 (1, 'Luxury Shoes ', 'Jl. Ring Road Utara, Ngringin, Condongcatur, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281\r\n');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transaksi`
---
-
-CREATE TABLE `transaksi` (
-  `id` int(11) NOT NULL,
-  `tanggal` datetime NOT NULL,
-  `barcode` varchar(10) NOT NULL,
-  `qty` varchar(10) NOT NULL,
-  `total_bayar` varchar(10) NOT NULL,
-  `jumlah_uang` varchar(10) NOT NULL,
-  `diskon` varchar(10) NOT NULL,
-  `pelanggan` int(11) DEFAULT NULL,
-  `nota` varchar(15) NOT NULL,
-  `kasir` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `transaksi`
---
-
-INSERT INTO `transaksi` (`id`, `tanggal`, `barcode`, `qty`, `total_bayar`, `jumlah_uang`, `diskon`, `pelanggan`, `nota`, `kasir`) VALUES
-(1, '2024-01-02 12:01:50', '1', '2', '1300000', '1300000', '', 1, '4SC0J6SYNCPE0ZA', 1),
-(2, '2024-01-02 12:02:56', '2', '3', '750000', '800000', '', 2, 'Z92LTS4X1U9BQRQ', 1),
-(3, '2024-01-02 12:03:23', '3', '45', '13500000', '14000000', '150000', 3, 'F2HDYKWKQ420PA1', 1);
 
 --
 -- Indexes for dumped tables
@@ -271,21 +207,9 @@ ALTER TABLE `stok_masuk`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `supplier`
---
-ALTER TABLE `supplier`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `toko`
 --
 ALTER TABLE `toko`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `transaksi`
---
-ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -296,55 +220,43 @@ ALTER TABLE `transaksi`
 -- AUTO_INCREMENT for table `kategori_produk`
 --
 ALTER TABLE `kategori_produk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `pengguna`
 --
 ALTER TABLE `pengguna`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `satuan_produk`
 --
 ALTER TABLE `satuan_produk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `stok_keluar`
 --
 ALTER TABLE `stok_keluar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `stok_masuk`
 --
 ALTER TABLE `stok_masuk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- AUTO_INCREMENT for table `supplier`
---
-ALTER TABLE `supplier`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `toko`
 --
 ALTER TABLE `toko`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `transaksi`
---
-ALTER TABLE `transaksi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
